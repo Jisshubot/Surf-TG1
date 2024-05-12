21af147126d758376ffa272d8f72a98445aee5a8
Jisshu developer <142872501+Jisshubot@users.noreply.github.com>
2024-05-12 14:35:01 +0530

    Update jisshu.py

--- bot/jisshu.py
+++ bot/jisshu.py
@@ -2,4 +2,4 @@ from os import getenv
 
 class Neha:
   THEMEE = getenv("THEMEE", "cerulean")
-  AUTH_CHANNEL = getenv("AUTH_CHANNEL").split(", ")
+  AUTH_CHANNEL = getenv("AUTH_CHANNEL").split("-1001835721669, -1002016524168, -1001888605542, -1002011809533")
